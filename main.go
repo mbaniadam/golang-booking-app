ec72cdec82a19f83be69188c93b3c0222c2f5179
Morteza Baniadam <mortalbm@gmail.com>
2023-07-28 12:26:56 +0330

    get data from input

--- main.go
+++ main.go
@@ -10,6 +10,19 @@ func main() {
 	var remainingTickets = 30
 	fmt.Println("Welcome to", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v tickets are still available!\n", conferenceTickets, remainingTickets)
+	// to print type of variables
+	fmt.Printf("conferenceName type is %T and conferenceTicketes is %T \n", conferenceName, conferenceTickets)
+	// we can also assign type to variables when we define it.
+	// in some cases we need for example only positiv number for integer
+	var userAge uint = 20
+	fmt.Printf("user age is %v years old!\n", userAge)
+	// got error with this >>> userAge = -1
+	// in the calculation due to the negative result, a random number will be printed. >>> userAge = userAge - 23
+	// fmt.Printf("userage is %v\n", userAge)
+
+	// another way to declare a variable
+	userBirthCity := "Tehran"
+	fmt.Println(userBirthCity)
 
 	var firstName string
 	var lastName string
@@ -26,21 +39,6 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTicket)
 
-	fmt.Printf("user %v %v booked %v tickets.\n", firstName, lastName, userTicket)
-
-	// to print type of variables
-	fmt.Printf("conferenceName type is %T and conferenceTicketes is %T and userTicket is %T\n", conferenceName, conferenceTickets, userTicket)
-
-	// we can also assign type to variables when we define it.
-	// in some cases we need for example only positiv number for integer
-	var userAge uint = 20
-	fmt.Printf("%v %v is %v years old!\n", firstName, lastName, userAge)
-	// got error with this >>> userAge = -1
-	// in the calculation due to the negative result, a random number will be printed. >>> userAge = userAge - 23
-	// fmt.Printf("userage is %v\n", userAge)
-
-	// another way to declare a variable
-	userBirthCity := "Tehran"
-	fmt.Println(userBirthCity)
+	fmt.Printf("Thank you %v %v for booking %v tickets.\n You will receive a confirrmation email at %v\n", firstName, lastName, userTicket, email)
 
 }
